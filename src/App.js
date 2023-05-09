b14c809b065dadcc2e7111de51b870b8d378b2cc
Andre <lifercode@gmail.com>
2023-05-09 10:54:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { Frame } from 'react95'
+import { React95Provider, OsProvider, OsWindow } from '@react-os/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Notes = ({ data }) => (
+  <OsWindow size={[540,480]}>
+    <Frame variant='field' style={{minHeight: '100%', width: '100%', padding: 20}}>
+      <p>
+        {data.label} is a grat window!
+      </p>
+    </Frame>
+  </OsWindow>
+)
 
-export default App;
+const App = () => (
+  <React95Provider>
+    <OsProvider
+      programs={[
+        {
+          menu: true,
+          desktop: true,
+          label: "Notes",
+          icon: "address_book_pad",
+          component: props => <Notes {...props} />
+        },
+        // more programs here...
+      ]}
+    />
+  </React95Provider>
+)
+
+
+export default App
